f425ee242dbf4232e024b1a6202853842562ac73
iahmedelbayaa <elbayaaahmed845@gmail.com>
2024-02-19 04:45:31 +0200

    Docs singleton lazy struc

--- creational/singleton/singleton-lazy.ts
+++ creational/singleton/singleton-lazy.ts
@@ -20,3 +20,10 @@ const instance1 = Singleton.getInstance();
 const instance2 = Singleton.getInstance();
 
 console.log(instance1 === instance2); // true - both instances refer to the same object
+
+// step 1 : create a class with a private static instance property
+// step 2 : create a private constructor to prevent instantiation
+// step 3 : create a static method to get the instance
+// step 4 : in the static method, check if the instance is null, if so, create a new instance
+// step 5 : return the instance
+// step 6 : create two instances and check if they are the same
